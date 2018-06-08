41f203e5a4cb31f32771605f147210ebd8c9f3b5
alek87 <allan_18tw@hotmail.com>
2018-06-08 20:09:15 -0300

    agregado template angular

---
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    animations: [routerTransition()]
+})
+export class DashboardComponent implements OnInit {
+    public alerts: Array<any> = [];
+    public sliders: Array<any> = [];
+
+    constructor() {
+        this.sliders.push(
+            {
+                imagePath: 'assets/images/slider7.jpg',
+                label: 'Municipalidad de Trelew',
+                text:
+                    'Robando desde tiempos inmemorables'
+            },
+            {
+                imagePath: 'assets/images/slider6.jpg',
+                label: 'Una intendencia con mala gente',
+                text: 'Minimo 4 antecedentes para poder ingresar a trabajar.'
+            },
+            {
+                imagePath: 'assets/images/slider3.jpg',
+                label: 'Y pronto una gobernacion peor',
+                text:
+                    'Haciendo tratos con corruptos y punteros para permanecer en el poder.'
+            }
+        );
+
+        this.alerts.push(
+            {
+                id: 1,
+                type: 'success',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            },
+            {
+                id: 2,
+                type: 'warning',
+                message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+            }
+        );
+    }
+
+    ngOnInit() {}
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
+}
